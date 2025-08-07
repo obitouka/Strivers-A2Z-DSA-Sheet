191b21990f2f00ff272725d349a7ae58e766e4d5
RoshanBehera <roshan08behera@gmail.com>
2025-08-07 13:16:44 +0530

    added clear and precised comments

--- src/B_Sorting_Techniques/a_sorting/Selection_Sort.java
+++ src/B_Sorting_Techniques/a_sorting/Selection_Sort.java
@@ -1,6 +1,7 @@
 /*
-Given an array of integers nums, sort the array in non-decreasing order using the selection sort algorithm and return the sorted array.
-A sorted array in non-decreasing order is an array where each element is greater than or equal to all previous elements in the array.
+QUESTION:
+Given an array of integers nums, sort the array in non-decreasing order using the selection sort algorithm 
+and return the sorted array.
 
 
 Examples:
